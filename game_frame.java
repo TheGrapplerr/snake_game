6fcf43dfe780947988c5ecfdf65a614b0ad6eb14
Gaurav Maurya <152024648+TheGrapplerr@users.noreply.github.com>
2024-10-22 18:26:16 +0530

    Create game_frame.java

---
+++ game_frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Game_frame extends JFrame {
+    Game_frame(){
+        this.add(new Game_panel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
